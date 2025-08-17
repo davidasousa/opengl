60cd12afa5dbab9cced4cf3ea38c4d8ebefc1c46
davidasousa <davidasousa335@gmail.com>
2025-08-17 16:57:35 -0500

    Lighting Scaling

--- src/ch14.cpp
+++ src/ch14.cpp
@@ -74,19 +74,19 @@ const char* objectFragmentShaderSource = R"GLSL(
 	uniform vec3 viewPos;
 
 	// Ambient
-	vec3 ambient = lightColor * material.ambient;
+	vec3 ambient = vec3(0.7) * (lightColor * material.ambient);
 
 	// Diffuse
 	vec3 norm = normalize(Normal);
 	vec3 lightDir = normalize(lightPos - FragPos);
 	float diff = max(dot(norm, lightDir), 0.0);
-	vec3 diffuse = lightColor * (diff * material.diffuse);
+	vec3 diffuse = vec3(0.7) * (lightColor * (diff * material.diffuse));
 	
 	// Specular
 	vec3 viewDir = normalize(viewPos - FragPos);
 	vec3 reflectDir = reflect(-lightDir, norm);
 	float spec = pow(max(dot(viewDir, reflectDir), 0.0), material.shininess);
-	vec3 specular = lightColor * (spec * material.specular);
+	vec3 specular = vec3(1.0) * (lightColor * (spec * material.specular));
 
 	vec3 result = (ambient + diffuse + specular);
 
